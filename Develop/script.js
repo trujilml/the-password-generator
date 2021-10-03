b5f8864c2fb734abb6493d117ba5933fbfd6c11e
trujilml <mickeytrj@gmail.com>
2021-10-03 16:31:26 -0500

    figured out const for characters, fixed together for if selections on browser, kinda came up with function

--- Develop/script.js
+++ Develop/script.js
@@ -5,54 +5,76 @@
 //lowercase letters
 //numbers 
 //special characters/symbols
-var passAlphaUpper = "";
-var passAlphaLower = "";
-var passNum = "";
-var passSymbol = "";
 
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var symbols = "`~!@#$%^&*>()?,_+/-=<[]\.{}|;':";
-var length;
 
-var selectionOfElements = "alphaUpper, alphaLower, numbers, symbols";
 
+const selectionOfCharacters = {
+  alphabetLower: 'abcdefghijklmnopqrstuvwxyz',
+  alphabetUpper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  symbols: '`~!@#$%^&*>()?,_+/-=<[]{}|;":',
+};
 
 window.alert("Welcome to the Password Generator! We grant your wish of wanting a secure password based on the criteria of your choice. Please click 'Generate Password' to get started!");
 
 
-//should be placed prior to length - figure out appropriate grouping together and ways to implement for password generator
-var passAlphaUpper = confirm("Next, would you like capital letters (ex: ABCDEFG...)?");
-var passAlphaLower = confirm("Would you like lowercase letters to be added (ex: abcdefg...)?");
-var passNum = confirm("How about numbers? These are added by single digit only (ex. 0-9).");
-var passSymbol = confirm("Finally, would you like to implement special characters/symbols (ex: !%#@^...)?");
-
-
 // length of password
 function generatePassword() {
-  window.prompt("Welcome! For your password, how long do you want it? It can be from 8 characters to 128 characters. Please type a number between the range of 8 and 128.");
+ // include it as function to confirm characters as applicable 
+  var passwordSet = ""; 
 
-if (length < 128 || length > 8); 
-return length;
-}
-
-
-// {
-//     window.prompt("For your next choice, would you like capital letters (ABC)?");
-
-//     if (alphaUpper < 128 || alphaUpper > 8);
-//     return alphaUpper;
-//   }
-
-// randomization of password with various characters when added 
-
-// function randomNum() {
-//   return [Math.floor(Math.random() * length)];
-// };
+  var length = window.prompt("Welcome! For your password, how long do you want it? It can be from 8 characters to 128 characters. Please type a number between the range of 8 and 128.");
 
 
+  if (length === "" || length === null) {
+    alert("You need to provide a valid length number ranging from 8 characters to 128 characters. Please type again.");
+    return generatePassword();
+  }
 
+  if (length < 8) {
+    alert("The length of your password must be at least 8 characters. Please type a greater value.");
+    return;
+  }
+
+  else if (length > 128) {
+    alert("The length of your password must not exceed 128 characters. Please type a lower value.");
+    return;
+  } 
+
+  
+
+  var alphabetLower = confirm("First, would you like lowercase letters (ex: abcdefg...)? If yes, click 'OK'. If no, click 'CANCEL'."); 
+  if (alphabetLower) {
+    passwordSet += selectionOfCharacters.alphabetLower;
+  };
+
+  var alphabetUpper = confirm("Next, would you like capital letters (ex: ABCDEFG...)? If yes, click 'OK'. If no, click 'CANCEL'.");
+  if (alphabetUpper) {
+    passwordSet += selectionOfCharacters.alphabetUpper;
+  };
+
+  var numbers = confirm("How about numbers? These are added by single digit only (ex. 0-9). If yes, click 'OK'. If no, click 'CANCEL'. ");
+  if (numbers) {
+    passwordSet += selectionOfCharacters.numbers;
+  };
+
+  var symbols = confirm("Finally, would you like to implement special characters/symbols (ex: !%#@^...)? If yes, click 'OK'. If no, click 'CANCEL'.");
+  if (symbols) {
+    passwordSet += selectionOfCharacters.symbols;
+  };
+
+  var password = "";
+
+  for (var i = 0; i < length; i++); 
+  {password = Math.floor[Math.random(selectionOfCharacters.length)];}
+  return password;
+
+};
+ 
+//figure out remainder of the function - 
+//allow for cancellation of password making if allowable 
+
+  
 
 
 
@@ -61,9 +83,6 @@ return length;
 
 //input is validated and only one selection is granted
 //password is displayed on the page or on a window prompt option 
-
-
-
 
 
 //leave these alone!!!
