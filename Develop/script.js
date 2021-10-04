fedc650e037d7f115d1a29d7476fc13ab8607695
trujilml <mickeytrj@gmail.com>
2021-10-03 23:50:31 -0500

    made another alert for symbols if not wanted by user even though user will need 1 for password, tried to figure out working function

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,26 @@
 // Assignment code here
 
-//will require the following
-//uppercase letters
-//lowercase letters
-//numbers 
-//special characters/symbols
-
-
-
-const selectionOfCharacters = {
-  alphabetLower: 'abcdefghijklmnopqrstuvwxyz',
-  alphabetUpper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  numbers: '0123456789',
-  symbols: '`~!@#$%^&*>()?,_+/-=<[]{}|;":',
-};
-
+//will placement be put later or up front?
 window.alert("Welcome to the Password Generator! We grant your wish of wanting a secure password based on the criteria of your choice. Please click 'Generate Password' to get started!");
 
-
-// length of password
 function generatePassword() {
- // include it as function to confirm characters as applicable 
+
   var passwordSet = ""; 
 
   var length = window.prompt("Welcome! For your password, how long do you want it? It can be from 8 characters to 128 characters. Please type a number between the range of 8 and 128.");
 
-
   if (length === "" || length === null) {
-    alert("You need to provide a valid length number ranging from 8 characters to 128 characters. Please type again.");
+    alert("You need to provide a valid number ranging from 8 characters to 128 characters for the password length. Please type a valid entry.");
     return generatePassword();
   }
 
-  if (length < 8) {
-    alert("The length of your password must be at least 8 characters. Please type a greater value.");
-    return;
+  if (length < 8 || length > 128) {
+    alert("The length of your password must be at least 8 characters and not exceed 128 characters. Please type a valid number ranging from 8 to 128.");
+    return generatePassword();
   }
 
-  else if (length > 128) {
-    alert("The length of your password must not exceed 128 characters. Please type a lower value.");
-    return;
-  } 
-
-  
-
+  //with correct length entered in prompt window
+  if (length >= 8 && length <= 128) {
   var alphabetLower = confirm("First, would you like lowercase letters (ex: abcdefg...)? If yes, click 'OK'. If no, click 'CANCEL'."); 
   if (alphabetLower) {
     passwordSet += selectionOfCharacters.alphabetLower;
@@ -61,14 +39,32 @@ function generatePassword() {
   var symbols = confirm("Finally, would you like to implement special characters/symbols (ex: !%#@^...)? If yes, click 'OK'. If no, click 'CANCEL'.");
   if (symbols) {
     passwordSet += selectionOfCharacters.symbols;
-  };
+  }
 
-  var password = "";
+  //ask if calling var characters are required again - how to repeat it rather than going back to reentering the length again - may become a function itself again.
+  while (alphabetLower != true && alphabetUpper !=true && numbers != true && symbols !=true) {
+    alert("You must choose at least one character type for your password! The available options are lowercase alphabet letters, uppercase alphabet letters, numbers, and symbols/special characters.")
+   return generatePassword();
+  }
+};
 
-  for (var i = 0; i < length; i++); 
-  {password = Math.floor[Math.random(selectionOfCharacters.length)];}
-  return password;
+window.addEventListener('load', function() {
+  generatePassword();
+});
 
+function generatePassword() {
+  
+  var passwordResult = "";
+
+  //var allowIt = {};
+
+  for (var i = passwordResult.length; i < length; i++); {
+    passwordResult += selectionOfCharacters.charAt = [Math.floor(Math.random() * selectionOfCharacters.length)];
+}
+{
+return passwordResult;
+}
+}
 };
  
 //figure out remainder of the function - 
@@ -86,6 +82,7 @@ function generatePassword() {
 
 
 //leave these alone!!!
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -98,5 +95,15 @@ function writePassword() {
 
 }
 
+//inserted characters needed for password - can be moved but mostly arrays stay on the bottom
+
+const selectionOfCharacters = {
+  alphabetLower: 'abcdefghijklmnopqrstuvwxyz',
+  alphabetUpper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  symbols: '`~!@#$%^&*>()?,_+/-=<[]{}|;":',
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
